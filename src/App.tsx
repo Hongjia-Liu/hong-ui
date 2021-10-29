d687955349ea83be76e2ccd67a0fedc459d10f30
Hongjia Liu <hongjia_liu@outlook.com>
2021-10-29 02:18:02 +0100

    add _reboot.scss and test our style

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <h2>Hello World</h2>
+        <h3>Hello World</h3>
+        <hr/>
+        <code>
+          const a = 'b'
+        </code>
         <p>
           Hong-UI Landing Page
         </p>
