22b2abeff2cabb6fb4b9ace6ba9677a0bf64d28a
Hongjia Liu <hongjia_liu@outlook.com>
2021-10-31 00:49:32 +0100

    add button style, part 1

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <Button disabled>Hello</Button>
-        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
-        <Button btnType={ButtonType.Link} href="www.google.com">Google</Button>
+        <Button> Hello </Button>
+        <Button disabled> Disabled Button </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com"> Baidu Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com" disabled> Disabled Link </Button>
 
         <p>
           Hong-UI Landing Page
