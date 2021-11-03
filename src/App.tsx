b5cbd6dc684d6d61c4245165afe9a6a330916a7d
Hongjia Liu <hongjia_liu@outlook.com>
2021-11-03 23:33:56 +0000

    Button component, part 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,12 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <Button> Hello </Button>
+        <Button className="custom"> Hello </Button>
         <Button disabled> Disabled Button </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
         <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Small Danger </Button>
-        <Button btnType={ButtonType.Link} href="http://www.google.com"> Baidu Link </Button>
-        <Button btnType={ButtonType.Link} href="http://www.google.com" disabled> Disabled Link </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com" target="_blank"> Google </Button>
+        <Button btnType={ButtonType.Link} href="http://www.google.com" disabled> Google  </Button>
 
         <p>
           Hong-UI Landing Page
